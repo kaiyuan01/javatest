33db7a6fe2cc317237614de2cd4c895c7476bd8b
kaiyuan01 <kaiyuan01@users.noreply.github.com>
2023-08-14 04:02:52 +0000

    u

--- myreact/my-react-app/src/index.js
+++ myreact/my-react-app/src/index.js
@@ -4,10 +4,24 @@ import ReactDOM from 'react-dom/client';
 
 const myFirstElement = <h1>Hello React!</h1>
 const mySecondElement = <h1>Hello React2!</h1>
+const myElement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
 root.render(mySecondElement);
+root.render(myElement);
 
 
 
