2db732da21e7207282913e7f205e4d334cb49859
kaiyuan01 <kaiyuan01@users.noreply.github.com>
2024-03-20 04:27:20 +0000

    java -cp .  com/dengsoft/java/Sample.java

--- com/dengsoft/java/Sample.java
+++ com/dengsoft/java/Sample.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//java -cp .  com/dengsoft/java/Sample.java
 public class Sample {
     public static void main(String[] args) {
         System.out.println(new Sample().toString());
